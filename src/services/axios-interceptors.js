a05c35aca82215cfaf74539f1b0752c8f4bf94ca
Jason Andersen Winfrey <jason.winfrey@gdn-commerce.com>
2022-04-18 11:23:38 +0700

    fix interceptor

--- src/services/axios-interceptors.js
+++ src/services/axios-interceptors.js
@@ -4,18 +4,15 @@ import { ApiPath } from "./api-path"
 export default function setupInterceptors(axios) {
   axios.interceptors.response.use(
     (response) => response,
-    async (error) => {
+    (error) => {
       const originalConfig = error.config
       if (error.config.url !== ApiPath.LOGIN_URL && error.response) {
         if (error.response.status === 401 && !originalConfig._retry) {
           console.log("refresh token by interceptor")
           originalConfig._retry = true
-          try {
-            await store.dispatch('auth/refreshToken')
-            return axios(originalConfig)
-          } catch (err) {
-            return Promise.reject(err)
-          }
+          store.dispatch('auth/refreshToken')
+            .then(() => axios(originalConfig))
+            .catch(err => Promise.reject(err))
         }
       }
       return Promise.reject(error)
